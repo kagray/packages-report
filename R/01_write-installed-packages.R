7624a20d1df6a11230dcb2472a563d4e2605248d
Kent Gray <kgray@smithfield.com>
2018-02-01 11:38:01 -0800

    added my libraries to data folder

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,7 +1,9 @@
 ## deja vu from yesterday!
-
+library(tidyverse)
 ## create a data frame of your installed packages
-
+(myPkg <- installed.packages() %>%
+  tbl_df() %>%
+  select(Package,LibPath,Version,Priority,Built))
 ## keep the variables
 ##   * Package
 ##   * LibPath
@@ -9,6 +11,7 @@
 ##   * Priority
 ##   * Built
 
+write_csv(myPkg,path = "data/installed-packages.csv")
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
