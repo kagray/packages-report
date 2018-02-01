c6778709428fdaa85c5dd71a436b9487b824f273
Kent Gray <kgray@smithfield.com>
2018-02-01 13:03:08 -0800

    fix the height of barplot

--- R/03_barchart-packages-built.R
+++ R/03_barchart-packages-built.R
@@ -4,12 +4,12 @@ library(here)
 ## read that csv into a data frame, then ...
 apt_freqtable <- read_csv(here("data","add-on-packages-freqtable.csv"))
 ## if you use ggplot2, code like this will work:
-ggplot(apt_freqtable, aes(x = Built, y = n)) +
+p <- ggplot(apt_freqtable, aes(x = Built, y = n)) +
   geom_bar(stat = "identity")
 
 ## write this barchart to figs/built-barchart.png
 ## if you use ggplot2, ggsave() will help
-ggsave(filename = here("figs","built-barchart.png"))
+ggsave(filename = here("figs","built-barchart.png"),p,height = 3)
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 
